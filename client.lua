86a83431f485ab1503a4712b1fcbe8393f98e3e5
Nikos <clan4fun@yahoo.gr>
2019-05-06 06:57:16 +0300

    Add files via upload

---
+++ client.lua
@@ -0,0 +1,24 @@
+------
+--» Custom Blips
+--» By FRANkiller13
+--» Do not remove credits !!
+--» Blips: https://wiki.gtanet.work/index.php?title=Blips
+------
+
+ESX                             = nil
+local Blips = {
+	{ name="An example", id=492,x=73.88, y=93.31, z=79.2},
+	--{ name="An example", id=492,x=73.88, y=93.31, z=79.2},
+}
+
+Citizen.CreateThread(function()
+	for _, item in pairs(Blips) do
+        item.blip = AddBlipForCoord(item.x, item.y, item.z)
+		SetBlipSprite(item.blip, item.id)
+		SetBlipColour(item.blip, 1)
+        SetBlipAsShortRange(item.blip, true)
+        BeginTextCommandSetBlipName("STRING")
+        AddTextComponentString(item.name)
+        EndTextCommandSetBlipName(item.blip)	
+	end
+end)
